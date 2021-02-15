5c9f7924dda964cd3f72427c1946a3d675c3154f
Hazim Arafa <hazimarafa80@gmail.com>
2021-02-14 23:06:50 -0800

    i swear if this shit works f2 is retarted

--- src/components/home/Index.tsx
+++ src/components/home/Index.tsx
@@ -3,4 +3,3 @@ export { default as About } from './about';
 export { default as Register } from './register';
 export { default as Faq } from './faq';
 export { default as Contact } from './contact';
-
