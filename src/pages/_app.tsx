e45e88b0034312df73fc9efdc8693b2f885663bf
Hazim Arafa <hazimarafa80@gmail.com>
2021-02-23 16:40:52 -0800

    using mailgun now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,6 +26,9 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 			<Head>
 				<title>{COMPANY_INFO.name}</title>
 				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.gstatic.com"/>
+    			<link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet"/>
+    			<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet"/>
 			</Head>
 
 			<Header />
