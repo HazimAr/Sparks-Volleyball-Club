ce4875b2772f0512d7c75c51348867c19c033bc0
Hazim Arafa <hazimarafa80@gmail.com>
2022-04-12 13:55:19 -0700

    Update register.tsx

--- src/components/home/register.tsx
+++ src/components/home/register.tsx
@@ -1,4 +1,12 @@
-import { Heading, HStack, SimpleGrid, VStack, Button } from "@chakra-ui/react";
+import {
+  Heading,
+  HStack,
+  VStack,
+  Button,
+  Text,
+  Image,
+  Center,
+} from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 
@@ -7,7 +15,49 @@ export default function () {
     <Container>
       <ContainerInside>
         <VStack>
-          <Heading as="h1">Why Us?</Heading>
+          <Heading as="h1">About Us</Heading>
+          <HStack spacing={0}>
+            <VStack w="100%">
+              <Heading>Who?</Heading>
+              <Text>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+                sit nam ad delectus tempora vel animi illum doloremque id
+                ducimus esse veniam rerum voluptatem nemo, voluptate obcaecati
+                aperiam sint quaerat.
+              </Text>
+            </VStack>
+            <Center w="100%">
+              <Image src="/cutouts/2.png" h="300px" />
+            </Center>
+          </HStack>
+          <HStack spacing={0}>
+            <Center w="100%">
+              <Image src="/cutouts/2.png" h="300px" />
+            </Center>
+            <VStack w="100%">
+              <Heading>What?</Heading>
+              <Text>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+                sit nam ad delectus tempora vel animi illum doloremque id
+                ducimus esse veniam rerum voluptatem nemo, voluptate obcaecati
+                aperiam sint quaerat.
+              </Text>
+            </VStack>
+          </HStack>
+          <HStack spacing={0}>
+            <VStack w="100%">
+              <Heading>Why?</Heading>
+              <Text>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+                sit nam ad delectus tempora vel animi illum doloremque id
+                ducimus esse veniam rerum voluptatem nemo, voluptate obcaecati
+                aperiam sint quaerat.
+              </Text>
+            </VStack>
+            <Center w="100%">
+              <Image src="/cutouts/2.png" h="300px" />
+            </Center>
+          </HStack>
           <Button>Register Now</Button>
         </VStack>
       </ContainerInside>
