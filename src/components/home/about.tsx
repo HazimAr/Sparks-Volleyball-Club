ab5bcd4892bd01ef3b6ae253920b9a020576f4f7
Hazim Arafa <hazimarafa80@gmail.com>
2022-04-13 09:12:50 -0700

    Update about.tsx

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -10,42 +10,47 @@ export default function () {
           <Heading as="h1" fontSize="7xl">
             About Us
           </Heading>
-          <HStack spacing={0}>
+          <HStack spacing={0} flexDir={{ base: "column", md: "row" }}>
             <VStack w="100%">
               <Heading>Who?</Heading>
               <Text>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                sit nam ad delectus tempora vel animi illum doloremque id
-                ducimus esse veniam rerum voluptatem nemo, voluptate obcaecati
-                aperiam sint quaerat.
+                Sparks Volleyball Club is committed to providing an excellent
+                volleyball experience that promotes hard work, great attitudes,
+                a genuine respect for the game, a “TEAM” concept at all times,
+                and leadership development for each athlete. SVBC will provide
+                the maximum opportunity for each athlete to train and compete at
+                their highest level.
               </Text>
             </VStack>
             <Center w="100%">
               <Image src="/cutouts/2.png" h="300px" />
             </Center>
           </HStack>
-          <HStack spacing={0}>
+          <HStack spacing={0} flexDir={{ base: "column-reverse", md: "row" }}>
             <Center w="100%">
               <Image src="/cutouts/3.png" h="300px" />
             </Center>
             <VStack w="100%">
               <Heading>What?</Heading>
               <Text>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                sit nam ad delectus tempora vel animi illum doloremque id
-                ducimus esse veniam rerum voluptatem nemo, voluptate obcaecati
-                aperiam sint quaerat.
+                Champion athletes aren’t just born - they’re selected, trained,
+                molded and tested. We hone raw talent to maturity with quality
+                coaching, primal workouts and a true love of the sport.
+                Teamwork, perseverance, sportsmanship - those are the hallmarks
+                of Sparks VBC. No more hoping for the “good coach” or following
+                coaches from club to club.
               </Text>
             </VStack>
           </HStack>
-          <HStack spacing={0}>
+          <HStack spacing={0} flexDir={{ base: "column", md: "row" }}>
             <VStack w="100%">
               <Heading>Why?</Heading>
               <Text>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                sit nam ad delectus tempora vel animi illum doloremque id
-                ducimus esse veniam rerum voluptatem nemo, voluptate obcaecati
-                aperiam sint quaerat.
+                Sparks Volley Ball Club was founded on the core belief of
+                teaching life lessons through the sport of volleyball. We have
+                some of the most experienced and sought-after coaches in the
+                region. Our state of the art 17,000 square foot facility is
+                equipped with the latest volleyball training equipment
               </Text>
             </VStack>
             <Center w="100%">
