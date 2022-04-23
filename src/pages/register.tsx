dda3f311d73a9ddb34240eb3a216513c6f3afa16
Hazim Arafa <hazimarafa80@gmail.com>
2022-04-22 23:34:28 -0700

    Update register.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,52 +3,77 @@ import {
   Heading,
   Image,
   Link,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
   SimpleGrid,
-  Text,
+  useDisclosure,
   VStack,
 } from "@chakra-ui/react";
 import Container from "@components/container";
 import ContainerInside from "@components/containerInside";
 import { Client } from "@notionhq/client";
 import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { useState } from "react";
 
 export default function Register({ forms }: { forms: QueryDatabaseResponse }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [image, setImage] = useState("");
   return (
-    <Container>
-      <ContainerInside>
-        <VStack>
-          <Heading as="h1">Register</Heading>
-          <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} gap={10}>
-            {forms.results.length &&
-              forms.results.map(
-                (form) =>
-                  // @ts-ignore
-                  form.properties.Name.title[0]?.plain_text &&
-                  // @ts-ignore
-                  form.properties.Form?.url &&
-                  // @ts-ignore
-                  form.properties.Description.rich_text[0]?.plain_text &&
-                  // @ts-ignore
-                  form.properties.Image.files[0]?.file?.url && (
-                    <Form
-                      key={form.id}
-                      // @ts-ignore
-                      name={form.properties.Name.title[0]?.plain_text}
-                      // @ts-ignore
-                      form={form.properties.Form?.url}
-                      description={
+    <>
+      <Container>
+        <ContainerInside>
+          <VStack>
+            <Heading as="h1" mb={20}>
+              Register
+            </Heading>
+            <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} gap={10}>
+              {forms.results.length &&
+                forms.results.map(
+                  (form) =>
+                    // @ts-ignore
+                    form.properties.Name?.title?.[0]?.plain_text &&
+                    // @ts-ignore
+                    form.properties.Form?.url &&
+                    // @ts-ignore
+                    form.properties.Description?.rich_text[0]?.plain_text &&
+                    // @ts-ignore
+                    form.properties.Image.files[0]?.file?.url && (
+                      <Form
+                        key={form.id}
                         // @ts-ignore
-                        form.properties.Description.rich_text[0]?.plain_text
-                      }
-                      // @ts-ignore
-                      img={form.properties.Image.files[0]?.file?.url}
-                    />
-                  )
-              )}
-          </SimpleGrid>
-        </VStack>
-      </ContainerInside>
-    </Container>
+                        name={form.properties.Name.title[0].plain_text}
+                        // @ts-ignore
+                        form={form.properties.Form.url}
+                        // @ts-ignore
+                        img={form.properties.Image.files[0].file.url}
+                        onClick={() => {
+                          // @ts-ignore
+                          setImage(form.properties.Image.files[0].file.url);
+                          onOpen();
+                        }}
+                      />
+                    )
+                )}
+            </SimpleGrid>
+          </VStack>
+        </ContainerInside>
+      </Container>
+      <Modal isOpen={isOpen} onClose={onClose} size="2xl">
+        <ModalOverlay />
+        <ModalContent bg="transparent">
+          <ModalBody>
+            <VStack>
+              <Image src={image} rounded="3xl" h="80vh" />
+              <Button mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </VStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
@@ -72,21 +97,23 @@ export async function getStaticProps() {
   };
 }
 
-function Form({ name, form, img, description }) {
+function Form({ name, form, img, ...props }) {
   return (
-    <Link href={form} isExternal>
-      <VStack
-        w="200px"
-        transition="all ease .2s"
-        _hover={{ transform: "scale(1.05)" }}
-      >
-        <Image src={img} borderRadius="3xl" alt={`${name} banner`} />
-        <VStack textAlign="center">
-          <Heading size="md">{name}</Heading>
-          <Text>{description}</Text>
+    <VStack
+      maxW="350px"
+      transition="transform ease .2s"
+      _hover={{
+        transform: "scale(1.1)",
+        cursor: "pointer",
+      }}
+      {...props}
+    >
+      <Image src={img} borderRadius="3xl" alt={`${name} banner`} />
+      <VStack textAlign="center">
+        <Link href={form} isExternal>
           <Button>Register</Button>
-        </VStack>
+        </Link>
       </VStack>
-    </Link>
+    </VStack>
   );
 }
