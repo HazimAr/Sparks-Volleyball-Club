0ab065c66c813f8b345677d4598da08417f5c2b2
Hazim Arafa <hazimarafa80@gmail.com>
2022-04-23 16:20:23 -0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -169,7 +169,7 @@ export default function Footer() {
 function FooterIcon({ icon, href }) {
   return (
     <Link href={href} isExternal>
-      <Text display="none">{href.split("/")[0]}</Text>
+      <Text display="none">{href.split("/")[1]}</Text>
       <Circle
         transition="all ease .3s"
         _hover={{ background: "white", color: "primary" }}
