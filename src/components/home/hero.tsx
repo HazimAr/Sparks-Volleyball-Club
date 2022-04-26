e7b51f157ef8d90b91a9a2055c083ed658291af1
Hazim Arafa <hazimarafa80@gmail.com>
2022-04-25 23:19:15 -0700

    Update hero.tsx

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -14,16 +14,42 @@ export default function () {
   return (
     <Container
       h="100vh"
-      backgroundImage={`/hero/${background}.png`}
       backgroundRepeat="no-repeat"
       backgroundSize="cover"
       backgroundPosition="top"
-      transition="background-image 1s ease"
     >
+      <style>
+        {`
+          @keyframes opacity {
+            0% {
+              opacity: 0;
+            }
+            10% {
+              opacity: 1;
+            }
+            90% {
+              opacity: 1;
+            }
+            100% {
+              opacity: 0;
+            }
+        `}
+      </style>
       {(() => {
         const imgs = [];
         for (let i = 0; i < 10; i++) {
-          imgs.push(<Image src={`/hero/${i}.png`} display="none" />);
+          imgs.push(
+            <Image
+              src={`/hero/${i}.png`}
+              visibility={background == i ? "visible" : "hidden"}
+              position="absolute"
+              w="100vw"
+              h="100vh"
+              objectFit="cover"
+              objectPosition="top"
+              animation={background == i && "opacity 3s ease forwards"}
+            />
+          );
         }
         return imgs;
       })()}
